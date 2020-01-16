44471b8bc73a82609562c5044bc1b320b31b5f84
fabiodc92 <fabiodecicco3@gmail.com>
2020-01-16 15:50:30 +0100

    Commit

--- src/app/pizza/pizza-list-item/pizza-list-item.component.ts
+++ src/app/pizza/pizza-list-item/pizza-list-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Pizza} from '../models/pizza';
 
 @Component({
   selector: 'app-pizza-list-item',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pizza-list-item.component.css']
 })
 export class PizzaListItemComponent implements OnInit {
+  pizza: Pizza = {
+        nome: 'Margherita',
+        descrizione: 'Pomodoro, mozzarella, olio'
+  };
 
   constructor() { }
 
